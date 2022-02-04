29afb91e20eac19ebeec0597857d1916bf65ffd2
XinghMp968 <31666974+singhmp968@users.noreply.github.com>
2022-02-04 22:22:21 +0530

    Redux: Actions, Reducers, Store-subscribing to store

--- src/componenets/App.js
+++ src/componenets/App.js
@@ -1,8 +1,38 @@
-import {data} from './data';
+  import {data} from './data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
-function App() {
-  console.log('daya\ is',data)
+import React from 'react';
+class App extends React.Component {
+  componentDidMount() {
+    
+    // WHEN WE DISPATCH ACTION IMMEDITELY after this call back is called
+    const {store} = this.props;
+    store.subscribe(()=>{
+      console.log('UPDATING');
+      // we dont we have use this as we have forceupdate as it is recommended
+      this.forceUpdate()
+    });
+
+    store.dispatch({
+      type:'ADD_MOVIES',
+      movies:data
+    });
+    console.log('STATE',this.props.store.getState())
+    // FLOW 
+    //1.store.dispatch({})
+    //2.THEN SUBSCRPTION IS CALLED  store.subscribe(()=>{})
+    //3.console.log('STATE',this.props.store.getState())
+    
+  
+  }
+  render(){
+    //4.THIS WILL EXECUTEBEACAUESE WE ARE FORCELLY UPDATIN THE COMP
+    console.log('RENDER')
+    //1.RENDER WILL EXECUTE as soon as the componenet 
+
+  const movies = this.props.store.getState();
+  console.log('Moviess==>',movies)
+  console.log('daya\ is',this.props)
   return (
     <div className="App">
         <Navbar />
@@ -13,14 +43,15 @@ function App() {
                       <div className="tab">Favourites</div>
                   </div>
                   <div className="list">
-                    {data.map(movie=>( // awways remenber here we need to have () bracket as we are passing arrow function not {} other wise you will get error
-                     <MovieCard  movie = {movie} />
+                    {movies.map((movie,index)=>( // awways remenber here we need to have () bracket as we are passing arrow function not {} other wise you will get error
+                     <MovieCard  movie = {movie} key={`movies-${index}`} />
                     ))}
                   </div>
             </div>
         </div>
     </div>
   );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,13 +5,19 @@ import { createStore } from 'redux';
 import './index.css';
 import App from './componenets/App';
 import movies from './reducers';
+// creatig the store
 const store=createStore(movies); // this expect and argument i.e reducer
 console.log('Store',store)
-console.log('Store',store.getState())
-
+// console.log('BEFORE Store STATE',store.getState())
+// store.dispatch({
+//   type:'ADD_MOVIES',
+//   movies:[{name:'superman'}]
+// }); // thi will take object as an argument
+// console.log('AFTER Store STATE',store.getState())
+{/* <App store={store} /> passing store in the app */}
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>,
   document.getElementById('root')
 );
